111fbf904905b11408ab60e6dd5e8c6070afff90
mingkew <mingkew@umich.edu>
2021-08-20 17:17:58 +0800

    fix

--- Codes/optimization/visulization.py
+++ Codes/optimization/visulization.py
@@ -1,13 +1,7 @@
-from src.visualize.visualization_cpp import show_track_1mag_csv_cpp_still
 import numpy as np
-from src.filter import Magnet_KF
-from src.solver import Solver
-from src.preprocess import Calibrate_Data, Reading_Data, LM_data_2mag
+from src.preprocess import Calibrate_Data
 from src.visualize import show_track_1mag_csv_cpp, show_track_2mag_csv_cpp
-from config import pSensor_smt, pSensor_large_smt, pSensor_median_smt, pSensor_small_smt, pSensor_imu, pSensor_imu_median
-import threading
-import os
-import matplotlib.pyplot as plt
+from config import pSensor_large_smt
 if __name__ == "__main__":
     LM_path = 'Path to the ground truth data collected by leap motion, in CSV'
     Reading_path = 'Path to the data to be investigated, in CSV'
